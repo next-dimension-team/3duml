058c5b8e0bcc5095d909526763eeefd70cb412d1
Zilo09 <dominik.zilka09@gmail.com>
2016-11-04 00:04:31 +0100

    Pridanie seedov

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,17 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(LifelinesSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+
+	public function run()
+	{
+		Model::unguard();
+	}
 }
