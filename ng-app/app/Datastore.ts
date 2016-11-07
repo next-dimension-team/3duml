40162b5b2ad1ca36b1bf0a6ed0fc3858bf48cafd
Ľubomír Jesze <lubomir.j184@gmail.com>
2016-11-07 21:09:25 +0100

    UML-58 Add Datastore class

---
+++ ng-app/app/Datastore.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { JsonApiDatastoreConfig, JsonApiDatastore } from 'angular2-jsonapi';
+
+@Injectable()
+@JsonApiDatastoreConfig({
+  baseUrl: 'http://localhost/api/',
+  models: {
+    //
+  }
+})
+export class Datastore extends JsonApiDatastore {
+
+    constructor(http: Http) {
+        super(http);
+    }
+
+}
