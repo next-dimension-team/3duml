b8d548d171529276205688322c5f00e26e1fdacd
mg-24 <mg-24@azet.sk>
2016-11-18 19:40:45 +0100

    Fix code style

--- app/Models/ExecutionSpecification.php
+++ app/Models/ExecutionSpecification.php
@@ -4,24 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ExecutionSpecification extends Model {
+class ExecutionSpecification extends Model
+{
+    protected $table = 'execution_specifications';
+    public $timestamps = true;
 
-	protected $table = 'execution_specifications';
-	public $timestamps = true;
+    public function startOccurenceSpecification()
+    {
+        return $this->hasOne('App\Models\OccurenceSpecification', 'start_occurence_specification_id');
+    }
 
-	public function startOccurenceSpecification()
-	{
-		return $this->hasOne('App\Models\OccurenceSpecification', 'start_occurence_specification_id');
-	}
+    public function finishOccurenceSpecification()
+    {
+        return $this->hasOne('App\Models\OccurenceSpecification', 'finish_occurence_specification_id');
+    }
 
-	public function finishOccurenceSpecification()
-	{
-		return $this->hasOne('App\Models\OccurenceSpecification', 'finish_occurence_specification_id');
-	}
-
-	public function interactionFragments()
-	{
-		return $this->morphMany('App\Models\InteractionFragment', 'fragmentable');
-	}
-
+    public function interactionFragments()
+    {
+        return $this->morphMany('App\Models\InteractionFragment', 'fragmentable');
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,24 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Message extends Model {
+class Message extends Model
+{
+    protected $table = 'messages';
+    public $timestamps = true;
 
-	protected $table = 'messages';
-	public $timestamps = true;
+    public function interaction()
+    {
+        return $this->belongsTo('App\Models\Interaction', 'interaction_id');
+    }
 
-	public function interaction()
-	{
-		return $this->belongsTo('App\Models\Interaction', 'interaction_id');
-	}
+    public function startOccurenceSpecification()
+    {
+        return $this->hasOne('App\Models\OccurenceSpecification', 'send_event_id');
+    }
 
-	public function startOccurenceSpecification()
-	{
-		return $this->hasOne('App\Models\OccurenceSpecification', 'send_event_id');
-	}
-
-	public function finishOccurenceSpecification()
-	{
-		return $this->hasOne('App\Models\OccurenceSpecification', 'receive_event_id');
-	}
-
+    public function finishOccurenceSpecification()
+    {
+        return $this->hasOne('App\Models\OccurenceSpecification', 'receive_event_id');
+    }
 }
