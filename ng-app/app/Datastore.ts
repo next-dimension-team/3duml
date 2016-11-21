ed99111012420973f36cb5e19b0ce7f76c07628f
Peter Zajac <Peter.Zajac11@sk.ibm.com>
2016-11-21 22:49:44 +0100

    Added configuration of Datastore

--- ng-app/app/Datastore.ts
+++ ng-app/app/Datastore.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { JsonApiDatastoreConfig, JsonApiDatastore } from 'angular2-jsonapi';
+import * as sd from './sequence-diagram/models';
 
 @Injectable()
 @JsonApiDatastoreConfig({
   baseUrl: 'http://localhost/api/',
   models: {
-    // add config of models 
+    'combined-fragments': sd.CombinedFragment,
+    'execution-specifications': sd.ExecutionSpecification,
+    'interactions': sd.Interaction,
+    'interaction-fragments': sd.InteractionFragment,
+    'interaction-operands': sd.InteractionOperand,
+    'lifelines': sd.Lifeline,
+    'messages': sd.Message,
+    'occurrence-specifications': sd.OccurrenceSpecification,
   }
 })
 export class Datastore extends JsonApiDatastore {
