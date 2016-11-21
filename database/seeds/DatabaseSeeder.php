d63206074bb3d117aec0520f5e474ffac0f82417
Matej Vitaz <mspiderv@gmail.com>
2016-11-21 13:36:24 +0100

    Database seeder disabled

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(LifelinesSeeder::class);
+        die("\nERROR: Please choose concrete database seeder with command\n$ php artisan db:seed --class SEEDER_NAME\n\n");
     }
 }
