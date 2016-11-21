aca5336732281fa386f5ec737fb4230f55893e3c
Matej Vitaz <mspiderv@gmail.com>
2016-11-21 14:26:00 +0100

    Seeds added

--- database/seeds/MinimalSequenceDiagramSeeder.php
+++ database/seeds/MinimalSequenceDiagramSeeder.php
@@ -11,6 +11,23 @@ class MinimalSequenceDiagramSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $sequenceDiagramInteraction = factory(\App\Models\Interaction::class)->create();
+
+        $lifelineA = factory(\App\Models\Lifeline::class)->create();
+        $lifelineB = factory(\App\Models\Lifeline::class)->create();
+
+        $occurenceSpecificationA = factory(\App\Models\OccurenceSpecification::class)->create([
+            'lifeline_id' => $lifelineA->id,
+        ]);
+
+        $occurenceSpecificationB = factory(\App\Models\OccurenceSpecification::class)->create([
+            'lifeline_id' => $lifelineB->id,
+        ]);
+
+        $message = factory(\App\Models\Message::class)->create([
+            'interaction_id' => $sequenceDiagramInteraction->id,
+            'send_event_id' => $lifelineA->id,
+            'receive_event_id' => $lifelineB->id,
+        ]);
     }
 }
