ea761850e602434c725aedfbd135f2722c11e006
Matej Vitaz <mspiderv@gmail.com>
2016-11-24 16:09:13 +0100

    App component proposed

--- ng-app/app/app.component.ts
+++ ng-app/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Interaction } from './sequence-diagram/models';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //
+
+  public openedSequenceDiagram: Interaction;
+  
+  openSequenceDiagram(sequenceDiagramModel: Interaction) {
+    this.openedSequenceDiagram = sequenceDiagramModel;
+  }
+
 }
