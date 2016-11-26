c3c9ce9193b8c496c4b10bf9dce816e0b8f760cb
Matej Vitaz <mspiderv@gmail.com>
2016-11-26 23:32:16 +0100

    Message model loading relationships proposed

--- ng-app/app/sequence-diagram/models/Message.ts
+++ ng-app/app/sequence-diagram/models/Message.ts
@@ -1,11 +1,19 @@
-import { JsonApiModelConfig, JsonApiModel, Attribute, BelongsTo } from 'angular2-jsonapi';
+import { JsonApiModelConfig, JsonApiModel, Attribute, BelongsTo, JsonApiDatastore } from 'angular2-jsonapi';
 import { OccurrenceSpecification } from './OccurrenceSpecification';
 import { Interaction } from './Interaction';
+import { Datastore } from '../../datastore';
 
 @JsonApiModelConfig({
     type: 'messages'
 })
 export class Message extends JsonApiModel {
+
+  protected datastore: JsonApiDatastore;
+
+  constructor(_datastore: JsonApiDatastore, data?: any) {
+    super(_datastore, data);
+    this.datastore = _datastore;
+  }
 
   @Attribute()
   name: string;
@@ -24,5 +32,9 @@ export class Message extends JsonApiModel {
 
   @BelongsTo()
   receiveEvent: OccurrenceSpecification;
+
+  public withRelationships(): Promise<Message> {
+    return this.datastore.findRecord(Message, this.id).toPromise();
+  }
 
 }
