13ad4ad7d55466a01c0d2d877c8f3e10f8d0ca40
Matej Vitaz <mspiderv@gmail.com>
2016-12-15 19:01:15 +0100

    Default seeder changed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        $this->call(MinimalSequenceDiagramSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        //$this->call(MinimalSequenceDiagramSeeder::class);
         //$this->call(ComplexSequenceDiagramSeeder::class);
+        $this->call(ATMSystemSequenceDiagramSeeder::class);
     }
 }
