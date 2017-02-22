ae24fa00772643baa69bc8335a2cc51e6882a2a0
Ľubomír Jesze <lubomir.j184@gmail.com>
2017-02-23 00:27:23 +0100

    Fix namespace in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -27,4 +27,3 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 }
-
