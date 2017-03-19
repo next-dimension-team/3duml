8feea09fc192a7cdf22f6e87d6e4189d996fa794
Peter Zajac <Peter.Zajac11@sk.ibm.com>
2017-03-19 20:47:24 +0100

    Seednuty Complicated Sequence Diagram

--- database/seeds/ComplicatedSequenceDiagramSeeder.php
+++ database/seeds/ComplicatedSequenceDiagramSeeder.php
@@ -12,232 +12,252 @@ use App\Models\ExecutionSpecification;
 
 /*
  * Original sequence diagram on link: https://postimg.org/image/urtp7bhup/
- * note: extension spec. are not exactly same in seed as there. 
+ * note: without execution specifiactions.
  */
 class ComplicatedSequenceDiagramSeeder extends Seeder
 {
-    /**
+     /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
+        
         // Create root interaction
-        $root = factory(Interaction::class)->create([
+        $rootInteraction = factory(Interaction::class)->create([
             'name' => 'Complicated Sequence Diagram',
         ]);
-        $rootF = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $root->id,
-            'fragmentable_type' => 'interaction',
-        ]);
-
-        // Create interactions
-        $interactionA = factory(Interaction::class)->create([
-            'name' => 'Interaction A',
-        ]);
-        $interactionFragmentA = tap(
-            factory(InteractionFragment::class)
-            ->make()
-            ->fragmentable()
-            ->associate($interactionA),
-            function ($interactionFragmentA) {
-                $interactionFragmentA->save();
+        $rootInteractionFragment = tap(
+            factory(InteractionFragment::class)->make()
+            ->fragmentable()->associate($rootInteraction),
+            function ($m) {
+                $m->save();
             }
         );
-        $interactionFragmentA->makeChildOf($rootF);
+        // Create interactions (Layer)
+        $interactionA = factory(Interaction::class)->create([
+            'name' => 'Interaction 1',
+        ]);
+        $interactionFragmentA = tap(
+            factory(InteractionFragment::class)->make()
+            ->fragmentable()->associate($interactionA)
+            ->parent()->associate($rootInteractionFragment),
+            function ($m) {
+                $m->save();
+            }
+        );
 
         // Create lifelines
-        $lifelineA = factory(Lifeline::class)->create([
-            'name' => 'Life A',
-            'order' => 1,
-        ]);
-        $lifelineB = factory(Lifeline::class)->create([
-            'name' => 'Life B',
-            'order' => 2,
-        ]);
-        $lifelineC = factory(Lifeline::class)->create([
-            'name' => 'Life C',
-            'order' => 3,
-        ]);
-        $lifelineD = factory(Lifeline::class)->create([
-            'name' => 'Life D',
-            'order' => 4,
-        ]);
+        $lifelineA = tap(
+            factory(Lifeline::class)->states('resetOrder')->make([
+                'name' => 'a: A',
+            ])
+            ->interaction()->associate($interactionA),
+            function ($m) {
+                $m->save();
+            }
+        );
 
-        // Lifeline A Occurrence Specifications
-        $ocA1 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineA->id,
-            'time' => 1,
-        ]);
-        $ocA2 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineA->id,
-            'time' => 5,
-        ]);
-        $ocA3 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineA->id,
-            'time' => 6,
-        ]);
+        $lifelineB = tap(
+            factory(Lifeline::class)->make([
+                'name' => 'b: B',
+            ])
+            ->interaction()->associate($interactionA),
+            function ($m) {
+                $m->save();
+            }
+        );
 
-        // Lifeline A Execution Specifications
-        $esA1 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocA1->id,
-            'finish_occurrence_specification_id' => $ocA2->id,
-        ]);
-        $esA1F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esA1->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esA1F->makeChildOf($interactionFragmentA);
-        
-        $esA2 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocA3->id,
-            'finish_occurrence_specification_id' => $ocA3->id,
-        ]);
-        $esA2F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esA2->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esA2F->makeChildOf($interactionFragmentA);
+        $lifelineC = tap(
+            factory(Lifeline::class)->make([
+                'name' => 'c: C',
+            ])
+            ->interaction()->associate($interactionA),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $lifelineD = tap(
+            factory(Lifeline::class)->make([
+                'name' => 'd: D',
+            ])
+            ->interaction()->associate($interactionA),
+            function ($m) {
+                $m->save();
+            }
+        );
 
-        // Lifeline B Occurrence Specifications
-        $ocB1 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineB->id,
-            'time' => 1,
-        ]);
-        $ocB2 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineB->id,
-            'time' => 2,
-        ]);
-        $ocB3 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineB->id,
-            'time' => 4,
-        ]);
+        // Lifeline A,B,C Occurrence Specifications
 
-        // Lifeline B Execution Specifications
-        $esB1 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocB1->id,
-            'finish_occurrence_specification_id' => $ocB3->id,
-        ]);
-        $esB1F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esB1->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esB1F->makeChildOf($interactionFragmentA);
+        //Lifeline A Occ
+        $occurrenceSpecificationA1 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 1,
+            ])
+            ->covered()->associate($lifelineA),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationA2 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 5,
+            ])
+            ->covered()->associate($lifelineA),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationA3 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 6,
+            ])
+            ->covered()->associate($lifelineA),
+            function ($m) {
+                $m->save();
+            }
+        );
 
-        // Lifeline C Occurrence Specifications
-        $ocC1 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineC->id,
-            'time' => 2,
-        ]);
-        $ocC2 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineC->id,
-            'time' => 3,
-        ]);
-        $ocC3 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineC->id,
-            'time' => 4,
-        ]);
-        $ocC4 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineC->id,
-            'time' => 5,
-        ]);
+        //Lifeline B Occ
+         $occurrenceSpecificationB1 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 1,
+            ])
+            ->covered()->associate($lifelineB),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationB2 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 2,
+            ])
+            ->covered()->associate($lifelineB),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationB3 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 4,
+            ])
+            ->covered()->associate($lifelineB),
+            function ($m) {
+                $m->save();
+            }
+        );
 
-        // Lifeline C Execution Specifications
-        $esC1 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocC1->id,
-            'finish_occurrence_specification_id' => $ocC3->id,
-        ]);
-        $esC1F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esC1->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esC1F->makeChildOf($interactionFragmentA);
-
-        $esC2 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocC4->id,
-            'finish_occurrence_specification_id' => $ocC4->id,
-        ]);
-        $esC2F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esC2->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esC2F->makeChildOf($interactionFragmentA);
-
-         // Lifeline D Occurrence Specifications
-        $ocD1 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineD->id,
-            'time' => 3,
-        ]);
-        $ocD2 = factory(OccurrenceSpecification::class)->create([
-            'lifeline_id' => $lifelineD->id,
-            'time' => 6,
-        ]);
-
-        // Lifeline D Execution Specifications
-        $esD1 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocD1->id,
-            'finish_occurrence_specification_id' => $ocD1->id,
-        ]);
-        $esD1F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esD1->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esD1F->makeChildOf($interactionFragmentA);
-
-        $esD2 = factory(ExecutionSpecification::class)->create([
-            'start_occurrence_specification_id' => $ocD2->id,
-            'finish_occurrence_specification_id' => $ocD2->id,
-        ]);
-        $esD2F = factory(InteractionFragment::class)->create([
-            'fragmentable_id' => $esD2->id,
-            'fragmentable_type' => 'execution_specification',
-        ]);
-        $esD2F->makeChildOf($interactionFragmentA);
+        //Lifeline C Occ
+         $occurrenceSpecificationC1 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 2,
+            ])
+            ->covered()->associate($lifelineC),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationC2 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 3,
+            ])
+            ->covered()->associate($lifelineC),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationC3 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 4,
+            ])
+            ->covered()->associate($lifelineC),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationC4 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 5,
+            ])
+            ->covered()->associate($lifelineC),
+            function ($m) {
+                $m->save();
+            }
+        );
+        // Lifeline D Occ
+        $occurrenceSpecificationD1 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 3,
+            ])
+            ->covered()->associate($lifelineD),
+            function ($m) {
+                $m->save();
+            }
+        );
+        $occurrenceSpecificationD2 = tap(
+            factory(OccurrenceSpecification::class)->make([
+                'time' => 6,
+            ])
+            ->covered()->associate($lifelineD),
+            function ($m) {
+                $m->save();
+            }
+        );
 
         // Messages
-        $message1 = factory(Message::class)->create([
+        factory(Message::class)->make([
             'name' => 'a()',
-            'sort' => 'synchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocA1->id,
-            'receive_event_id' => $ocB1->id,
-        ]);
-        $message2 = factory(Message::class)->create([
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationA1)
+            ->receiveEvent()->associate($occurrenceSpecificationB1)
+            ->save();
+
+        factory(Message::class)->make([
             'name' => 'b()',
-            'sort' => 'synchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocB2->id,
-            'receive_event_id' => $ocC1->id,
-        ]);
-        $message3 = factory(Message::class)->create([
-            'name' => 'e()',
-            'sort' => 'synchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocC2->id,
-            'receive_event_id' => $ocD1->id,
-        ]);
-        $message4 = factory(Message::class)->create([
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationB2)
+            ->receiveEvent()->associate($occurrenceSpecificationC1)
+            ->save();
+
+        factory(Message::class)->make([
             'name' => 'c()',
-            'sort' => 'synchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocC3->id,
-            'receive_event_id' => $ocB3->id,
-        ]);
-        $message5 = factory(Message::class)->create([
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationC3)
+            ->receiveEvent()->associate($occurrenceSpecificationB3)
+            ->save();
+
+         factory(Message::class)->make([
             'name' => 'd()',
-            'sort' => 'asynchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocC4->id,
-            'receive_event_id' => $ocA2->id,
-        ]);
-        $message6 = factory(Message::class)->create([
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationC4)
+            ->receiveEvent()->associate($occurrenceSpecificationA2)
+            ->save();  
+
+         factory(Message::class)->make([
+            'name' => 'e()',
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationC2)
+            ->receiveEvent()->associate($occurrenceSpecificationD1)
+            ->save();
+
+         factory(Message::class)->make([
             'name' => 'f()',
-            'sort' => 'asynchCall',
-            'interaction_id' => $root->id,
-            'send_event_id' => $ocA3->id,
-            'receive_event_id' => $ocD2->id,
-        ]);
+            'sort' => 'synchCall'
+        ])
+            ->interaction()->associate($interactionA)
+            ->sendEvent()->associate($occurrenceSpecificationA3)
+            ->receiveEvent()->associate($occurrenceSpecificationD2)
+            ->save();          
     }
 }
