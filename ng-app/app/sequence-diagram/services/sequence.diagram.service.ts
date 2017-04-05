06bb410e6f71a02cd28c13c26997d1db72510967
Matej Vitaz <mspiderv@gmail.com>
2017-04-05 16:08:31 +0200

    Bug s posuvanim lifelines fixed

--- ng-app/app/sequence-diagram/services/sequence.diagram.service.ts
+++ ng-app/app/sequence-diagram/services/sequence.diagram.service.ts
@@ -164,7 +164,7 @@ export class SequenceDiagramService {
     });
     this.inputService.onMouseMove((event) => {
       if (this.draggingLifeline && this.editMode.valueOf() == true)
-        this.draggingLifeline.left = event.offsetX - 436;
+        this.draggingLifeline.left = event.offsetX - 436 - 75;
     });
     this.inputService.onMouseUp((event) => {
       if (moveBool && this.selectedLifeline != null) {
