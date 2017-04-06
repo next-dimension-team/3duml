bf350693ae5f39bdd34c9f41d2c4a6212a0e086f
Peter Zajac <Peter.Zajac11@sk.ibm.com>
2017-04-06 15:24:03 +0200

    Nemoze sa pridat prazdne meno elementov.

--- ng-app/app/menu/components/edit-dialog.component.ts
+++ ng-app/app/menu/components/edit-dialog.component.ts
@@ -1,16 +1,19 @@
-import { Component, EventEmitter } from '@angular/core'
+import { Component, EventEmitter, ChangeDetectorRef } from '@angular/core'
 import { MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-edit-dialog',
-  template: `<h2>{{title}}</h2>
+  template: `
+   <h2>{{title}}</h2>
       <p>{{message}}</p>
       <md-input-container>
-        <input mdInput value="{{placeholder}}" (keyup.enter)="onOk.emit(input.value); dialog.close()" #input>
+        <input mdInput value="{{placeholder}}" (keyup.enter)="submitInput(input.value)" #input>
       </md-input-container>
-      <button md-button (click)="onOk.emit(input.value); dialog.close()">OK</button>
+      <button md-button [disabled]="inputEmpty(input.value)" (click)="submitInput(input.value)">OK</button>
       <button md-button (click)="onNo.emit(); dialog.close()"><md-icon>close</md-icon> Cancel</button>`
 })
+
+// treba pridat este zmenu spravy na synchronnu a asynchr.
 export class EditDialogComponent {
 
   public title: string;
@@ -19,5 +22,28 @@ export class EditDialogComponent {
   onOk = new EventEmitter();
   onNo = new EventEmitter();
 
-  constructor( public dialog: MdDialogRef<any>) { }
+  inputEmpty(input : string): boolean {
+
+    if (input.length == 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  submitInput(input: string): void {
+
+    if (this.inputEmpty(input)) {
+      return;
+    }
+    this.onOk.emit(input); 
+    this.dialog.close();
+  }
+  
+  constructor( public dialog: MdDialogRef<any>, private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+
 }
