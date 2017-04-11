03705c554c330300df61e293d294472cbe8836ef
Matej Vitaz <mspiderv@gmail.com>
2017-04-11 18:26:28 +0200

    Enable prod mode

--- ng-app/app/environment.ts
+++ ng-app/app/environment.ts
@@ -16,8 +16,10 @@ let PROVIDERS: any[] = [
 // https://github.com/angular/angular/blob/86405345b781a9dc2438c0fbe3e9409245647019/TOOLS_JS.md
 let _decorateModuleRef = <T>(value: T): T => { return value; };
 
+// Enable prod mode always
+enableProdMode();
+
 if ('production' === ENV) {
-  enableProdMode();
 
   // Production
   _decorateModuleRef = (modRef: any) => {
